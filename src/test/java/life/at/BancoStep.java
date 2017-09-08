4229be651fc72e7ed70ae5fd4eb1be728fc156bb
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-09-08 12:04:07 -0400

    Adicionado Stories de Exemplo
    Signed-off-by: Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>

---
+++ src/test/java/life/at/BancoStep.java
@@ -0,0 +1,10 @@
+package life.at;
+
+import org.jbehave.core.steps.Steps;
+
+/**
+ * @author Mateus Gabi Moreira <mateusgabimoreira@gmail.com>
+ *         on 06/09/2017.
+ */
+public class BancoStep extends Steps {
+}
